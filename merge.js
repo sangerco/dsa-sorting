71612ad06293a95af053621bb4f857462d5ff96b
Dan Sanger <sangerco@yahoo.com>
2023-06-16 18:51:51 -0600

    initial commit

---
+++ merge.js
@@ -0,0 +1,34 @@
+function merge(arr1, arr2) {
+    let resArr = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length) {
+        if(arr1[i] < arr2[j]) {
+            resArr.push(arr1[i]);
+            i++;
+        } else {
+            resArr.push(arr2[j]);
+            j++;
+        }
+    }
+    while(i < arr1.length){
+        resArr.push(arr1[i]);
+        i++;
+    }
+    while(j < arr2.length){
+        resArr.push(arr2[j]);
+        j++;
+    }
+    return resArr;
+}
+
+function mergeSort(arr) {
+    if(arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right)
+}
+
+module.exports = { merge, mergeSort};
